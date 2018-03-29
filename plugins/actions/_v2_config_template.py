e28965dda4b87e7b5cd80802280f880f4a42dff2
Laurence <laurencegill@users.noreply.github.com>
2018-03-29 17:10:42 +0100

    update for ansible 2.5

--- plugins/actions/_v2_config_template.py
+++ plugins/actions/_v2_config_template.py
@@ -34,7 +34,8 @@ import yaml
 
 
 from ansible.plugins.action import ActionBase
-from ansible.utils.unicode import to_bytes, to_unicode
+#from ansible.utils.unicode import to_bytes, to_unicode
+from ansible.module_utils._text import to_bytes, to_text as to_unicode
 from ansible import constants as C
 from ansible import errors
 
